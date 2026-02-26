b515a1ac85721233ec458193c958ebfaa213bcaa
omarelafi00 <omarelafi00@gmail.com>
2026-02-26 16:29:30 +0200

    main character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,15 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
 
     private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
 
     }
     private void OnEnable(){
@@ -19,12 +24,26 @@ public class PlayerController : MonoBehaviour
         PlayerInput();
     }
     private void FixedUpdate(){
+        AdjustPlayerFactingDirection();
         Move();
     }
     private void PlayerInput(){
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
     private void Move(){
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
-}
+    
+    
+    private void AdjustPlayerFactingDirection() {
+    if (movement.x > 0) {
+        mySpriteRender.flipX = false;
+    } 
+    else if (movement.x < 0) {
+        mySpriteRender.flipX = true;
+    }
+    }
+    }
